f49ce576a948142d4367ff0ec82cd2f1f2272eab
Pranita Gughane <69386793+pranita1999@users.noreply.github.com>
2023-04-18 13:27:33 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+print("Welcome to Php");
+
+print("\n");
+
+print `ifconfig`;
+
+?>
+
+
+
+
+<?php
+#echo '<pre>';
+print("Welcome to Php");
+
+#print("\n");
+
+#print_r (`ifconfig`);
+#print_r(explode(`ifconfig`));
+echo $_SERVER['SERVER_ADDR'];
+?>
